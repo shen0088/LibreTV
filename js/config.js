33b4388d55f437bf4b48e935aca8a3a08f3c848e
shen0088 <niuyulan9@gmail.com>
2025-07-06 07:19:27 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -110,6 +110,10 @@ const API_SITES = {
         api: 'https://ikunzyapi.com/api.php/provide/vod',
         name: 'iKun资源'
     },
+    kzy: {
+        api: 'https://www.ckzy1.com',
+        name: 'CK资源'
+    },
     testSource: {
         api: 'https://www.example.com/api.php/provide/vod',
         name: '空内容测试源',
